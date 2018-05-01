858dcc05a9ecb98621c093be3e1b70c91f01df2a
Gabe Hargrave <gkhargrave@gmail.com>
2018-04-30 17:43:06 -0700

    Add tests for Queue

--- src/js/a_structures/b_queue.js
+++ src/js/a_structures/b_queue.js
@@ -1,53 +1,67 @@
 /*
+====================================
+= QUEUE
+====================================
 
-QUEUE
-
-Abstract data type
-FIFO - First in, first out
-Collection of elements with enqueue and dequeue operations.
-Note that there is a natural order. Elements are removed in the reverse order of their addition.
+- Abstract data type
+- FIFO - First in, first out
+- Collection of elements with enqueue and dequeue operations.
+- Note that there is a natural order. Elements are removed in the reverse order of their addition.
 
 DO NOT use an array and the native push/shift method in your
 implementation. Use an object as the underlying data structure.
 
-*** Operations:
+====================================
+= Operations
+====================================
 
 myQueue.enqueue(value)
-=> count of queue
-add value to collection
+  => count of queue
+  add value to collection
 
 myQueue.dequeue()
-=> oldest element added collection
-Remove item so that it is no longer in collection
+  => oldest element added collection
+  Remove item so that it is no longer in collection
 
 myQueue.peek()
-=> oldest element added collection
-Similiar to dequeue, but do not remove element from collection
+  => oldest element added collection
+  Similiar to dequeue, but do not remove element from collection
 
 myQueue.count()
-=> number of elements in queue
+  => number of elements in queue
 
-*** Additional Exercises:
+====================================
+= Additional Exercises
+====================================
 
 Modify your queue to take a max capacity and return a string if you try to add an element when there's no more room:
-myQueue.enqueue(value)
-=> "Max capacity already reached. Remove element before adding a new one."
+  myQueue.enqueue(value)
+  => "Max capacity already reached. Remove element before adding a new one."
 
-Create a contains method to check if a value is in the queue:
-myQueue.contains('findme')
-=> true/false
-What's the time complexity?
+Create a 'contains' method to check if a value is in the queue:
+  myQueue.contains('findme')
+  => true/false
+  What's the time complexity?
 
-Create an until method to get the number of dequeues until you get to a certain value:
-queue values - (first)2-5-7-3-6-9(last)
-myQueue.until(7)
-=> 3
-What's the time complexity?
+Create an 'until' method to get the number of dequeues until you get to a certain value:
+  queue values - (first)2-5-7-3-6-9(last)
+  myQueue.until(7)
+  => 3
+  What's the time complexity?
 
- */
+====================================
+= Bonus
+====================================
+
+1. Implement a queue using two stacks.
+
+2. Implement a double-ended queue, with the following methods: enqueueLeft, dequeueLeft, enqueueRight, dequeueRight.
+
+3. Given a tree, print out the value of each node in breadth-first order using a queue data structure.
+*/
 
 function Queue(capacity) {
-  this.capacity = capacity;
+  this.capacity = Math.max(capacity, 2);
   this.data = {};
   this.firstIndex = 0;
   this.lastIndex = -1;
@@ -106,13 +120,4 @@ Queue.prototype.until = function(val) {
   return -1;
 };
 
-/*
-*** Exercises:
-
-1. Implement a queue using two stacks.
-
-2. Implement a double-ended queue, with the following methods: enqueueLeft, dequeueLeft, enqueueRight, dequeueRight.
-
-3. Given a tree, print out the value of each node in breadth-first order using a queue data structure.
-
- */
+module.exports = Queue;
---
+++ src/js/a_structures/b_queue.spec.js
@@ -0,0 +1,128 @@
+const Queue = require('./b_queue');
+
+describe('Queue', () => {
+  const DEFAULT_SIZE = 5;
+  let queue;
+
+  beforeEach(() => {
+    queue = new Queue(DEFAULT_SIZE);
+  });
+
+  describe('constructor', () => {
+    it('should correctly generate a new Queue object', () => {
+      expect(queue.capacity).toEqual(DEFAULT_SIZE);
+      expect(queue.data).toMatchObject({});
+      expect(queue.firstIndex).toEqual(0);
+      expect(queue.lastIndex).toEqual(-1);
+    });
+
+    it('should have a minimum capacity of 2', () => {
+      queue = new Queue(-1);
+      expect(queue.capacity).toEqual(2);
+    });
+  });
+
+  describe('enqueue', () => {
+    it('should insert the value and return the new index', () => {
+      let result = queue.enqueue(100);
+      expect(result).toEqual(0);
+      expect(queue.count()).toEqual(1);
+      result = queue.enqueue(200);
+      expect(result).toEqual(1);
+      expect(queue.count()).toEqual(2);
+    });
+
+    it('should not add the value and should return a string if capacity has been reached', () => {
+      queue = new Queue(2);
+      queue.enqueue(1);
+      queue.enqueue(2);
+      let result = queue.enqueue(3);
+      expect(queue.count()).toEqual(2);
+      expect(typeof result).toEqual('string');
+    });
+  });
+
+  describe('dequeue', () => {
+    it('should remove and return the first-added value', () => {
+      queue.enqueue(1);
+      queue.enqueue(2);
+      expect(queue.count()).toEqual(2);
+      let result = queue.dequeue();
+      expect(result).toEqual(1);
+      expect(queue.count()).toEqual(1);
+      result = queue.dequeue();
+      expect(result).toEqual(2);
+      expect(queue.count()).toEqual(0);
+    });
+
+    it('should return null if the queue is empty', () => {
+      const result = queue.dequeue();
+      expect(result).toEqual(null);
+    });
+  });
+
+  describe('peek', () => {
+    it('should return the first-added value', () => {
+      queue.enqueue(1);
+      queue.enqueue(2);
+      expect(queue.count()).toEqual(2);
+      let result = queue.peek();
+      expect(result).toEqual(1);
+      expect(queue.count()).toEqual(2);
+      result = queue.peek();
+      expect(result).toEqual(1);
+      expect(queue.count()).toEqual(2);
+    });
+
+    it('should return null if the queue is empty', () => {
+      expect(queue.peek()).toEqual(null);
+    });
+  });
+
+  describe('count', () => {
+    it('should return the current number of items', () => {
+      queue.enqueue(1);
+      expect(queue.count()).toEqual(1);
+      queue.enqueue(1);
+      expect(queue.count()).toEqual(2);
+      queue.dequeue();
+      expect(queue.count()).toEqual(1);
+      queue.dequeue();
+      expect(queue.count()).toEqual(0);
+    });
+  });
+
+  describe('contains', () => {
+    it('should return true if the value is in the queue', () => {
+      queue.enqueue(1);
+      queue.enqueue(2);
+      queue.enqueue(3);
+      expect(queue.contains(2)).toEqual(true);
+    });
+
+    it('should return false if the value is not in the queue', () => {
+      queue.enqueue(1);
+      queue.enqueue(2);
+      queue.enqueue(3);
+      expect(queue.contains(4)).toEqual(false);
+    });
+  });
+
+  describe('until', () => {
+    it('should return the correct number of steps to reach the value', () => {
+      queue.enqueue(1);
+      queue.enqueue(2);
+      queue.enqueue(3);
+      queue.enqueue(4);
+      expect(queue.until(3)).toEqual(2);
+    });
+
+    it('should return -1 if the value is not found', () => {
+      queue.enqueue(1);
+      queue.enqueue(2);
+      queue.enqueue(3);
+      queue.enqueue(4);
+      expect(queue.until(5)).toEqual(-1);
+    });
+  });
+});
