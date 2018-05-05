5696a0e2ff5d9aa235d2b89bc5d38151f96c9213
Gabe Hargrave <gkhargrave@gmail.com>
2018-05-05 10:41:15 -0700

    Add tests for Recursion problems

--- src/js/a_structures/c_recursion.js
+++ src/js/a_structures/c_recursion.js
@@ -1,65 +1,63 @@
 // 1. Write a function that loops through the numbers n down to 0.
 // If you haven't done so try using a while loop to do this.
-function loopDownW(n) {
+export const loopDownI = (n) => {
+  const arr = [];
   while (n >= 0) {
-    console.log('n: ' + n);
-    n -= 1;
+    arr.push(n--);
   }
-}
-loopDownW(10);
+  return arr;
+};
 
 // 2. Next, try looping just like above except using recursion
-function loopDown(n) {
-  console.log('n: ' + n);
+export const loopDownR = (n, accum = []) => {
+  accum.push(n);
   if (n === 0) {
-    return 0;
+    return accum;
   }
-  return loopDown(n - 1);
-}
-loopDown(10);
+  return loopDownR(n - 1, accum);
+};
 
 // 3.Write a function 'exponent' that takes two arguments base, and expo,
 // uses a while loop to return the exponenet value of the base.
-function expW(base, e) {
+export const expI = (base, exp) => {
   let val = base;
-  while (e > 1) {
+  while (exp > 1) {
     val *= base;
-    e -= 1;
+    exp -= 1;
   }
   return val;
-}
-console.log('expW(3, 3) = ' + expW(3, 3));
-console.log('expW(4, 3) = ' + expW(4, 3));
+};
 
 // 4. Write a function 'RecursiveExponent' that takes two arguments base,
 // and expo, recursively returns exponent value of the base.
-function expR(base, e) {
+export const expR = (base, e) => {
   if (e === 1) {
     return base;
   }
   return base * expR(base, e - 1);
-}
-console.log('expR(3, 3) = ' + expR(3, 3));
-console.log('expR(4, 3) = ' + expR(4, 3));
+};
 
 // 5. Write a function 'recursiveMultiplier' that takes two arguments,
 // 'arr and num', and multiplies each arr value by num and returns an array of the values.
-function recursiveMultiplier(arr, num) {
-  let multArr = [];
-  function mult(arr, num, i) {
+export const recursiveMultiplier = (arr, num = 1) => {
+  const multArr = [];
+  const mult = (arr, num, i) => {
     if (i === arr.length) {
       return multArr;
     }
     multArr.push(arr[i] * num);
     return mult(arr, num, i + 1);
-  }
+  };
   return mult(arr, num, 0);
-}
-console.log('recursiveMultiplier([1, 2, 3], 2): ' + recursiveMultiplier([1, 2, 3], 2));
+};
 
 // 6. Write a function 'recursiveReverse' that takes an array and uses
 // recursion to return its contents in reverse
-function recursiveReverse(arr) {
+export const recursiveReverse = (arr = []) => {
+  if (!Array.isArray(arr)) {
+    arr = [];
+  }
+  
   let revArr = [];
   function rev(arr, i) {
     if (i < 0) {
@@ -69,5 +67,4 @@ function recursiveReverse(arr) {
     return rev(arr, i - 1);
   }
   return rev(arr, arr.length - 1);
-}
-console.log('recursiveReverse([1, 2, 3]): ' + recursiveReverse([1, 2, 3]));
+};
---
+++ src/js/a_structures/c_recursion.spec.js
@@ -0,0 +1,79 @@
+import {
+  expI,
+  expR,
+  loopDownI,
+  loopDownR,
+  recursiveMultiplier,
+  recursiveReverse,
+} from './c_recursion';
+
+describe('Recursion', () => {
+  describe('loopDownI', () => {
+    it('should return an array with the correct number of elements', () => {
+      let start = 5;
+      let arr = loopDownI(start);
+      expect(arr.length).toEqual(start + 1);
+    });
+
+    it('should have "n" as the first value, and 0 as the last', () => {
+      let start = 5;
+      let arr = loopDownI(start);
+      expect(arr[0]).toEqual(start);
+      expect(arr[start]).toEqual(0);
+    });
+  });
+
+  describe('loopDownR', () => {
+    it('should return an array with the correct number of elements', () => {
+      let start = 5;
+      let arr = loopDownR(start);
+      expect(arr.length).toEqual(start + 1);
+    });
+
+    it('should have "n" as the first value, and 0 as the last', () => {
+      let start = 5;
+      let arr = loopDownR(start);
+      expect(arr[0]).toEqual(start);
+      expect(arr[start]).toEqual(0);
+    });
+  });
+
+  describe('expI', () => {
+    it('should return the correct exponent', () => {
+      expect(expI(0, 3)).toEqual(0);
+      expect(expI(2, 3)).toEqual(8);
+    });
+  });
+
+  describe('expR', () => {
+    it('should return the correct exponent', () => {
+      expect(expR(0, 3)).toEqual(0);
+      expect(expR(2, 3)).toEqual(8);
+    });
+  });
+
+  describe('recursiveMultiplier', () => {
+    it('should correctly multiply the array by various values', () => {
+      let arr = [1, 2, 3];
+      expect(recursiveMultiplier(arr, 2)).toEqual([2, 4, 6]);
+      expect(recursiveMultiplier(arr, 3)).toEqual([3, 6, 9]);
+    });
+
+    it('should return the same values if no "num" arg is provided', () => {
+      let arr = [1, 2, 3];
+      expect(recursiveMultiplier(arr)).toEqual(arr);
+    });
+  });
+
+  describe('recursiveReverse', () => {
+    it('should correctly reverse an array of any values', () => {
+      expect(recursiveReverse([1, 'a', 2, 'b'])).toEqual(['b', 2, 'a', 1]);
+    });
+
+    it('should return an empty array if input is incorrect', () => {
+      expect(recursiveReverse()).toEqual([]);
+      expect(recursiveReverse(null)).toEqual([]);
+      expect(recursiveReverse('asdf')).toEqual([]);
+    });
+  });
+});
