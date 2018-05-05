313d7b9b568af49f411fbb013e50da7573a563af
Gabe Hargrave <gkhargrave@gmail.com>
2018-05-05 16:41:33 -0700

    Add tests for LinkedList

--- src/js/a_structures/d_linkedList.js
+++ src/js/a_structures/d_linkedList.js
@@ -1,52 +1,58 @@
 /*
-
-LINKED LIST
+====================================
+= LINKED LIST
+====================================
 
 Comprised of nodes that represent a sequence.
 Each node is composed of data and a reference/link to the next node.
 
-*** Operations:
+====================================
+= Operations
+====================================
 
 ** Part 1
 
 myList.forEach(callbackFn)
-invoke callback function with the value of each node
+  invoke callback function with the value of each node
 
 myList.print()
-=> string with all values in list (ex: '0, 1, 2, 3')
+  => string with all values in list (ex: '0, 1, 2, 3')
 
 myList.insertAfter(refNode, value)
-=> new node
-insert new node associated with value passed in after refNode
+  => new node
+  insert new node associated with value passed in after refNode
 
 myList.removeAfter(refNode)
-=> removed node
-remove node after the refNode
+  => removed node
+  remove node after the refNode
 
 myList.insertHead(value)
-=> new head
-insert new head node at the beginning of the list with the value passed in
+  => new head
+  insert new head node at the beginning of the list with the value passed in
 
 myList.removeHead()
-=> removed head node
-remove the head node of the linked list
+  => removed head node
+  remove the head node of the linked list
 
 myList.findNode(value)
-=> first node that has a value matching what was passed in
+  => first node that has a value matching what was passed in
 
-* Optimization:
+====================================
+= Optimization
+====================================
+
 Say we have a linked list that has 100 items and we want to add
 an item to the very end. How would you do that with your current
 implementation? How can you modify the data structure to add an
 item to the end in constant time?
 
 myList.appendToTail(value)
-=> new tail node
-add a new tail node at the end of the list with the associated value passed in
+  => new tail node
+  add a new tail node at the end of the list with the associated value passed in
 
 myList.removeTail()
-=> removed tail node
-remove the tail node from the list
+  => removed tail node
+  remove the tail node from the list
 
 ** Part 2
 
@@ -61,116 +67,134 @@ How can we modify our data structures (Node and Linked List classes) so that we 
 Once you've come up with a plan, implement the following methods.
 
 myList.insertBefore(refNode, value)
-=> new node inserted
-insert new node with associated value before refNode
+  => new node inserted
+  insert new node with associated value before refNode
 
 myList.removeBefore(refNode)
-=> removed node
-remove node before the refNode passed in
+  => removed node
+  remove node before the refNode passed in
 
-*** Additional Exercises:
+====================================
+= Additional Exercises
+====================================
 
 Implement a circularly linked list:
 https://en.wikipedia.org/wiki/Linked_list#Circularly_linked_list
 
 Reimplement stack and queue data structures using linked lists.
-
 */
-const assert = require('assert');
 
-function Node(value, next = null) {
-  this.value = value;
-  this.next = next;
-}
-
-function LinkedList(headValue) {
-  if (headValue) {
-    this.head = new Node(headValue, null);
-    this.tail = this.head;
-  } else {
-    this.head = null;
-    this.tail = null;
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
   }
 }
 
-LinkedList.prototype.getHeadNode = function() {
-  return this.head;
-};
-
-LinkedList.prototype.getTailNode = function() {
-  return this.tail;
-};
-
-LinkedList.prototype.addToStart = function(value) {
-  let node = new Node(value);
-  node.next = this.head;
-  this.head = node;
-  return node;
-};
-
-LinkedList.prototype.addToEnd = function(value) {
-  let node = new Node(value, null);
-  this.tail.next = node;
-  this.tail = node;
-  return node;
-};
-
-LinkedList.prototype.addAfter = function(after, value) {
-  let node = new Node(value, after.next);
-  after.next = node;
-  if (this.tail === after) {
-    this.tail = node;
-  }
-  return node;
-};
-
-LinkedList.prototype.removeHead = function() {
-  let node = this.head;
-  this.head = node.next;
-  return node;
-};
-
-LinkedList.prototype.removeAfter = function(after) {
-  // set 'next' of node 'after' to 'next' of 'after.next'
-  let next = after.next;
-  if (next) {
-    after.next = next.next;
-  }
-};
-
-LinkedList.prototype.print = function(value) {
-  let node = this.head;
-  let str = '';
-
-  while (node) {
-    str = str.concat(node.value);
-    node = node.next;
-    if (node) {
-      str = str.concat(', ');
+export default class LinkedList {
+  constructor(headValue) {
+    if (headValue) {
+      this._head = new Node(headValue);
+      this._tail = this._head;
+      this._count = 1;
+    } else {
+      this._head = null;
+      this._tail = null;
+      this._count = 0;
     }
   }
-  console.log(str);
-  return str;
-};
 
-let ll = new LinkedList('a');
-let a = ll.getHeadNode();
-assert.equal(ll.print(), 'a');
-let b = ll.addToEnd('b');
-assert.equal(ll.print(), 'a, b');
-let c = ll.addToEnd('c');
-assert.equal(ll.print(), 'a, b, c');
-let d = ll.addAfter(b, 'd');
-assert.equal(ll.print(), 'a, b, d, c');
-assert.deepEqual(b.next, d);
-ll.removeAfter(b);
-assert.equal(ll.print(), 'a, b, c');
-assert.deepEqual(b.next, c);
-let newHead = ll.addToStart('0');
-assert.equal(ll.print(), '0, a, b, c');
-assert.deepEqual(ll.getHeadNode(), newHead);
-assert.deepEqual(newHead.next, a);
-assert.deepEqual(ll.removeHead(), newHead);
-assert.deepEqual(ll.getHeadNode(), a);
-let e = ll.addAfter(c, 'e');
-assert.deepEqual(ll.getTailNode(), e);
+  findNode(value) {
+    let node = this._head;
+    while(node) {
+      if (node.value === value) {
+        return node;
+      }
+      node = node.next;
+    }
+    return null;
+  }
+
+  append(value) {
+    const newTail = new Node(value);
+    if (this._tail) {
+      this._tail.next = newTail;
+    }
+    this._tail = newTail;
+    if (!this._head) {
+      this._head = newTail;
+    }
+    this._count += 1;
+    return newTail;
+  }
+
+  insertHead(value) {
+    const prevHead = this._head;
+    this._head = new Node(value, prevHead);
+    this._count += 1;
+    return this._head;
+  }
+
+  insertAfter(refNode, value) {
+    if (!refNode || !this.findNode(refNode.value)) {
+      return null;
+    }
+
+    const newNode = new Node(value, refNode.next);
+    refNode.next = newNode;
+    this._count += 1;
+    return newNode;
+  }
+
+  removeHead() {
+    const node = this._head;
+    this._head = node.next;
+    this._count -= 1;
+    return node;
+  }
+
+  removeAfter(refNode) {
+    if (!refNode || !refNode.next) {
+      return null;
+    }
+
+    const node = refNode.next;
+    refNode.next = node.next;
+    this._count -= 1;
+    return node;
+  }
+
+  forEach(cb) {
+    let node = this._head;
+    while(node) {
+      cb(node.value);
+      node = node.next;
+    }
+  }
+
+  print() {
+    let node = this._head;
+    let str = `${node.value}`;
+    while (node.next) {
+      str = `${str}, ${node.next.value}`;
+      node = node.next;
+    }
+    return str;
+  }
+
+  get headNode() {
+    return this._head;
+  }
+
+  get head() {
+    return this._head && this._head.value;
+  }
+
+  get tail() {
+    return this._tail && this._tail.value;
+  }
+
+  get count() {
+    return this._count;
+  }
+}
---
+++ src/js/a_structures/d_linkedList.spec.js
@@ -0,0 +1,176 @@
+import LinkedList from './d_linkedList';
+
+const values = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+const buildList = (len = 0) => {
+  const ll = new LinkedList();
+  for (let i = 0; i < len; i++) {
+    ll.append(values[i]);
+  }
+  return ll;
+};
+
+describe('LinkedList', () => {
+  describe('constructor', () => {
+    it('creates a new list with provided value as head', () => {
+      const ll = new LinkedList('a');
+      expect(ll.head).toBe('a');
+      expect(ll.tail).toBe('a');
+    });
+
+    it('creates a new list with empty head/tail', () => {
+      const ll = buildList();
+      expect(ll.head).toBe(null);
+      expect(ll.tail).toBe(null);
+    });
+  });
+
+  describe('count', () => {
+    it('returns the correct count of nodes', () => {
+      const ll = buildList();
+      expect(ll.count).toBe(0);
+      ll.append('a');
+      expect(ll.count).toBe(1);
+      ll.append('b');
+      expect(ll.count).toBe(2);
+    });
+  });
+
+  describe('findNode', () => {
+    it('should return the node with the given value', () => {
+      const ll = buildList(3);
+      const node = ll.findNode('b');
+      expect(node.value).toBe('b');
+      expect(node.next.value).toBe('c');
+    });
+
+    it('should return null if the node is not found', () => {
+      const ll = buildList(3);
+      const node = ll.findNode('d');
+      expect(node).toBe(null);
+    });
+  });
+
+  describe('append', () => {
+    it('correctly adds a new value as the tail', () => {
+      const ll = buildList(1);
+      expect(ll.head).toBe('a');
+      expect(ll.tail).toBe('a');
+      expect(ll.count).toBe(1);
+      ll.append('b');
+      expect(ll.head).toBe('a');
+      expect(ll.tail).toBe('b');
+      expect(ll.count).toBe(2);
+    });
+
+    it('returns the new tail Node', () => {
+      const ll = buildList(1);
+      const tail = ll.append('b');
+      expect(ll.tail).toBe(tail.value);
+    });
+
+    it('sets new tail as head if list is currently empty', () => {
+      const ll = buildList();
+      expect(ll.head).toBe(null);
+      expect(ll.tail).toBe(null);
+      ll.append('a');
+      expect(ll.head).toBe('a');
+      expect(ll.tail).toBe('a');
+    });
+  });
+
+  describe('insertHead', () => {
+    it('should correctly add and return the new head node', () => {
+      const ll = buildList(3);
+      expect(ll.count).toBe(3);
+      const newHead = ll.insertHead('aa');
+      expect(ll.count).toBe(4);
+      expect(newHead.value).toBe('aa');
+      expect(newHead.next.value).toBe('a');
+      expect(ll.head).toBe('aa');
+      expect(ll.headNode.next.value).toBe('a');
+      expect(ll.headNode).toEqual(newHead);
+    });
+  });
+
+  describe('insertAfter', () => {
+    it('should correctly insert a node after the target node, and return the new node', () => {
+      const ll = buildList(1);
+      ll.append('c');
+      expect(ll.count).toBe(2);
+      const refNode = ll.findNode('a');
+      const newNode = ll.insertAfter(refNode, 'b');
+      expect(ll.count).toBe(3);
+      expect(refNode.next.value).toBe('b');
+      expect(newNode.next.value).toBe('c');
+    });
+
+    it('should correctly insert the node as the new tail', () => {
+      const ll = buildList(2);
+      const refNode = ll.findNode('b');
+      const newNode = ll.insertAfter(refNode, 'c');
+      expect(refNode.next.value).toBe('c');
+      expect(newNode.next).toBe(null);
+    });
+
+    it('should return null if refNode is not valid', () => {
+      const ll = buildList(3);
+      expect(ll.count).toBe(3);
+      expect(ll.insertAfter({}, 'd')).toBe(null);
+      expect(ll.insertAfter(undefined, 'd')).toBe(null);
+      expect(ll.count).toBe(3);
+    });
+  });
+
+  describe('removeHead', () => {
+    it('should correctly remove and return the current head node', () => {
+      const ll = buildList(3);
+      expect(ll.count).toBe(3);
+      const head = ll.removeHead();
+      expect(ll.count).toBe(2);
+      expect(head.value).toBe('a');
+      expect(ll.head).toBe('b');
+      expect(ll.headNode.next.value).toBe('c');
+    });
+  });
+
+  describe('removeAfter', () => {
+    it('should correctly remove and return the node if it is found', () => {
+      const ll = buildList(4);
+      const refNode = ll.findNode('b');
+      expect(ll.count).toBe(4);
+      const removedNode = ll.removeAfter(refNode);
+      expect(ll.count).toBe(3);
+      expect(refNode.next.value).toBe('d');
+      expect(removedNode.value).toBe('c');
+    });
+
+    it('should return null if "refNode" is invalid', () => {
+      const ll = buildList(3);
+      expect(ll.removeAfter({})).toBe(null);
+    });
+  });
+
+  describe('forEach', () => {
+    it('calls a function on all current nodes', () => {
+      const cb = jest.fn();
+      const ll = buildList(3);
+      ll.forEach(cb);
+      expect(cb.mock.calls.length).toBe(3);
+      expect(cb.mock.calls[0][0]).toBe('a');
+      expect(cb.mock.calls[1][0]).toBe('b');
+      expect(cb.mock.calls[2][0]).toBe('c');
+    });
+  });
+
+  describe('print', () => {
+    it('should return a string with all values', () => {
+      const ll = buildList(3);
+      const str = ll.print();
+      ['a', 'b', 'c'].forEach((char) =>
+        expect(str.includes(char)).toBe(true)
+      );
+      expect(str.includes('d')).toBe(false);
+    });
+  });
+});
