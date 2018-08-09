37b6fad35832b99f87dde52ee423b98d4c63b137
Gabe Hargrave <gkhargrave@gmail.com>
2018-08-09 09:19:43 -0700

    Merge sort

--- src/js/b_sorting/d_mergeSort.js
+++ src/js/b_sorting/d_mergeSort.js
@@ -1,16 +1,22 @@
 /*
-MERGE SORT
+====================================
+= MERGE SORT
+====================================
 
-*** Description
+Description
 
 Merge sort employs a divide and conquer strategy - merge two sorted subarrays into one sorted array.
-Recursive top-down approach:
-Recursively break down array into two subarrays and sort them
-  recursively. Subarrays are broken down until they have only 1 element (implying they are sorted).
-Iterative bottom-up approach:
-Split array into sublists of size 1, merge adjacent sublists into sorted lists, repeat until no more sublists.
 
-*** Exercises
+Recursive top-down approach:
+- Recursively break down array into two subarrays and sort them recursively.
+- Subarrays are broken down until they have only 1 element (implying they are sorted).
+
+Iterative bottom-up approach:
+- Split array into sublists of size 1, merge adjacent sublists into sorted lists, repeat until no more sublists.
+
+====================================
+= Exercises
+====================================
 
 - Implement recursive merge sort (you might want to write a helper function to handle the merge step)
 - Implement iterative merge sort
@@ -18,10 +24,9 @@ Split array into sublists of size 1, merge adjacent sublists into sorted lists, 
 - Identify space complexity
 
 - Modify function to take comparator function. specify default if
-  not provided (check out native Array.sort comparator function for reference)
-- Use your comparator function to verify that your sort is stable
-  by taking input:
-  [{value: 15}, {value: 10, order: 1}, {value: 10, order: 2}]
+    not provided (check out native Array.sort comparator function for reference)
+- Use your comparator function to verify that your sort is stable by taking input:
+    [{value: 15}, {value: 10, order: 1}, {value: 10, order: 2}]
 
 Optimization:
 - Refactor your iterative solution to be a natural merge sort.
@@ -32,54 +37,76 @@ ex:
 input array: [ 1 2 4 5 9 ]
 subarrays for regular merge sort: [ [1], [2], [4], [5], [9] ]
 subarrays for natural merge sort: [ [1,2], [4,5], [9] ]
-*/
 
-/*
 Properties:
-O(n) extra space for iterative solution
-O(n·log(n)) time (for worst and best)
-stable - the only stable O(n·log(n)) sorting algorithm
-not adaptive
+- O(n) extra space for iterative solution
+- O(n·log(n)) time (for worst and best)
+- stable - the only stable O(n·log(n)) mergeSortIterativeng algorithm
+- not adaptive
+
 Use cases:
 If stabilty is a requirement and using extra space is no concern,
-merge sort is great because it's simple to implement, it's the only stable O(nlog(n)) sorting algorithm.
+merge sort is great because it's simple to implement, it's the only stable O(nlog(n)) mergeSortIterativeng algorithm.
 */
-const assert = require('assert');
+
+// Rough results based on Arrays of randomized ints:
+//
+// Recursive:
+// @ 25 elements: 0.210ms
+// @ 100 elements: 0.101ms
+// @ 1000 elements: 3.326ms
+// @ 10000 elements: 5.463ms
+// @ 50000 elements: 17.600ms
+//
+// Iterative:
+// @ 25 elements: 0.237ms
+// @ 100 elements: 0.110ms
+// @ 1000 elements: 3.189ms
+// @ 10000 elements: 10.023ms
+// @ 50000 elements: 31.725ms
 
 function merge(a, b) {
-  const targetLen = a.length + b.length;
-  let result = [];
-  let ia = 0;
-  let ib = 0;
+  const combinedLen = a.length + b.length;
+  let merged = [];
+  let idxA = 0;
+  let idxB = 0;
 
-  // check ia vs ib, and push lower number into result
-  while (result.length < targetLen) {
-    if (ia === a.length) {
-      result.push(b[ib++]);
-    } else if (ib === b.length) {
-      result.push(a[ia++]);
+  while (merged.length < combinedLen) {
+    if (idxB === b.length || a[idxA] < b[idxB]) {
+      merged.push(a[idxA++]);
     } else {
-      const val = (a[ia] < b[ib]) ? a[ia++] : b[ib++];
-      result.push(val);
+      merged.push(b[idxB++]);
     }
   }
-
-  return result;
+  return merged;
 }
 
-function mergeSort(arr) {
-  if (arr.length <= 1) {
+// a.k.a. "top down" merge sort
+export function mergeSortRecursive(arr) {
+  if (arr.length === 1) {
     return arr;
   }
 
-  let mid = arr.length / 2;
-  let left = mergeSort(arr.slice(0, mid));
-  let right = mergeSort(arr.slice(mid));
+  const mid = arr.length / 2;
+  const left = mergeSortRecursive(arr.slice(0, mid));
+  const right = mergeSortRecursive(arr.slice(mid));
   return merge(left, right);
 }
 
-let result1 = merge([1, 4, 5], [2, 3, 5, 6, 7, 8]);
-assert.deepEqual(result1, [1, 2, 3, 4, 5, 5, 6, 7, 8]);
+// a.k.a. "bottom up" merge sort
+export function mergeSortIterative(arr) {
+  const len = arr.length;
 
-let result2 = mergeSort([14, 7, 3, 12, 9, 11, 6, 2]);
-assert.deepEqual(result2, [2, 3, 6, 7, 9, 11, 12, 14]);
+  for (let i = 1; i <= len; i *= 2) {
+    for (let j = i; j < len; j += i * 2) {
+      const lo = j - i;
+      const hi = j + i;
+      const left = arr.slice(lo, j);
+      const right = arr.slice(j, hi);
+      const merged = merge(left, right);
+      arr.splice(lo, merged.length, ...merged);
+    }
+  }
+
+  return arr;
+}
