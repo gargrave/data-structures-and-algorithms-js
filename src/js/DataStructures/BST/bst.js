e1918b428b6565937a2e28121397451a3ba43dbb
Gabe Hargrave <gkhargrave@gmail.com>
2018-08-23 21:23:50 -0700

    WIP BST tests and implentation

--- src/js/DataStructures/BST/bst.js
+++ src/js/DataStructures/BST/bst.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 /*
 ====================================
 = BINARY SEARCH TREES
@@ -47,24 +48,16 @@ removeNode(value)
   => node
   Remove node from tree.
 
-checkIfFull()
+isFull()
   => true/false
   A binary tree is full if every node has either zero or two children
     (no nodes have only one child)
 
-checkIfBalanced()
+isBalanced()
   => true/false
   For this exercise, let's say that a tree is balanced if the minimum
   height and the maximum height differ by no more than 1. The height
     for a branch is the number of levels below the root.
-
-====================================
-= Additional Exercises
-====================================
-
-A binary search tree was created by iterating over an array and
-inserting each element into the tree. Given a binary search tree
-with no duplicates, how many different arrays would result in the creation of this tree.
 */
 
 export default class BinarySearchTree {
@@ -127,207 +120,114 @@ export default class BinarySearchTree {
     return this.right.findMax();
   }
 
-  checkIfFull() {
+  findDepth(depth = 1) {
+    const dl = this.left ? this.left.findDepth(depth + 1) : depth;
+    const dr = this.right ? this.right.findDepth(depth + 1) : depth;
+    return Math.max(dl, dr);
+  }
+
+  isFull() {
     if ((this.left && !this.right) || (!this.left && this.right)) {
+      // only one child; definitely not full
       return false;
     } else if (this.left && this.right) {
-      return true;
+      // two children; need to check both children recursively
+      return this.left.isFull() && this.right.isFull();
     }
-    return false;
+    // no children; this tree is full
+    return true;
+  }
+
+  isBalanced() {
+    const dl = this.left ? this.left.findDepth() : 0;
+    const dr = this.right ? this.right.findDepth() : 0;
+    return Math.abs(dl - dr) <= 1;
+  }
+
+  traverseDepthFirstInOrder(callback) {
+    this.left && this.left.traverseDepthFirstInOrder(callback);
+    callback(this.value);
+    this.right && this.right.traverseDepthFirstInOrder(callback);
+  }
+
+  traverseDepthFirstPreOrder(callback) {
+    callback(this.value);
+    this.left && this.left.traverseDepthFirstPreOrder(callback);
+    this.right && this.right.traverseDepthFirstPreOrder(callback);
+  }
+
+  traverseDepthFirstPostOrder(callback) {
+    this.left && this.left.traverseDepthFirstPostOrder(callback);
+    this.right && this.right.traverseDepthFirstPostOrder(callback);
+    callback(this.value);
+  }
+
+  /* eslint-disable max-len */
+
+  /*
+  BST node removal:
+    1. If a node has no children, we can simply remove this node from the tree. No changes necessary.
+    2. If a node has only a single child, we simply remove this node and replace it with said child.
+    3. If a node has two children, this is where it gets tricky:
+      1. Find the in-order successor (i.e. the left-most child in the right sub-tree)
+      2. Copy the *value* of this node into the node being deleted.
+      3. The successor node can now be easily deleted, because it will only have 0 or 1 children.
+   */
+  remove(value) {
+    // TODO: implement this, yo!
   }
 }
 
-BinarySearchTree.prototype.remove = function(value) {
-  const traverse = (current, parent = null) => {
-    if (current.value === value) {
-      if (parent) {
-        if (parent.right && parent.right.value === value) {
-          // 'current' is the parent's 'right'
-          if (current.left && current.right) {
-            // do cool stuff
-            const min = current.getMinNode();
-            current.remove(min.value);
-            current.value = min.value;
-          } else if (current.left) {
-            // only has a 'left' child -> set parent's child to current's 'left'
-            parent.right = current.left;
-          } else if (current.right) {
-            // only has a 'right' child -> set parent's child to current's 'right'
-            parent.right = current.right;
-          } else {
-            parent.right = null;
-          }
-        } else if (parent.left && parent.left.value === value) {
-          // 'current' is the parent's 'left'
-          if (current.left && current.right) {
-            // do cool stuff
-            const min = current.getMinNode();
-            current.remove(min.value);
-            current.value = min.value;
-          } else if (current.left) {
-            // only has a 'left' child -> set parent's child to current's 'left'
-            parent.left = current.left;
-          } else if (current.right) {
-            // only has a 'right' child -> set parent's child to current's 'right'
-            parent.left = current.right;
-          } else {
-            parent.left = null;
-          }
-        }
-      } else {
-        // this is a root
-      }
-    } else if (current.value < value) {
-      if (current.right) {
-        traverse(current.right, current);
-      }
-    } else {
-      if (current.left) {
-        traverse(current.left, current);
-      }
-    }
-  };
-  traverse(this);
-  return this;
-};
-
-BinarySearchTree.prototype.deleteMin = function() {
-  if (this.left) {
-    // if we still have a left node, traverse further
-    this.left = this.left.deleteMin();
-    return this;
-  } else if (this.right) {
-    // if we have only a right node, this node is the min
-    // so we need to set the parent's 'left' to this one's 'right'
-    return this.right;
-  } else {
-    // if no left or right, this is a leaf, so simply return null
-    return null;
-  }
-};
-
-BinarySearchTree.prototype.deleteMax = function() {
-  if (this.right) {
-    // if we still have a right node, traverse further
-    this.right = this.right.deleteMax();
-    return this;
-  } else if (this.left) {
-    // if we have only a left node, this node is the max
-    // so we need to set the parent's 'right' to this one's 'left'
-    return this.left;
-  } else {
-    // if no left or right, this is a leaf, so simply return null
-    return null;
-  }
-};
-
-BinarySearchTree.prototype.toArrayInOrder = function() {
-  let arr = [];
-  const traverse = (node) => {
-    if (node.left) {
-      traverse(node.left);
-    }
-    arr.push(node.value);
-    if (node.right) {
-      traverse(node.right);
-    }
-  };
-  traverse(this);
-  return arr;
-};
-
-BinarySearchTree.prototype.traverseDepthFirstInOrder = function(fn) {
-  // implement me...
-  if (this.left) {
-    this.left.traverseDepthFirst_inOrder(fn);
-  }
-  fn(this);
-  if (this.right) {
-    this.right.traverseDepthFirst_inOrder(fn);
-  }
-};
-
-BinarySearchTree.prototype.traverseDepthFirstPreOrder = function(fn) {
-  fn(this);
-  if (this.left) {
-    this.left.traverseDepthFirst_inOrder(fn);
-  }
-  if (this.right) {
-    this.right.traverseDepthFirst_inOrder(fn);
-  }
-};
-
-BinarySearchTree.prototype.traverseDepthFirstPostOrder = function(fn) {
-  if (this.left) {
-    this.left.traverseDepthFirst_inOrder(fn);
-  }
-  if (this.right) {
-    this.right.traverseDepthFirst_inOrder(fn);
-  }
-  fn(this);
-};
-
-// BinarySearchTree.prototype.checkIfFull = function() {
-//   // a tree is full if every node has exactly 0 or 2 child nodes; no node can have a single child
-//   if ((!this.left && this.right) || (this.left && !this.right)) {
-//     return false;
-//   }
-//   if (!this.left && !this.right) {
-//     return true;
-//   }
-//   return this.right.checkIfFull() && this.left.checkIfFull();
+// BinarySearchTree.prototype.remove = function(value) {
+//   const traverse = (current, parent = null) => {
+//     if (current.value === value) {
+//       if (parent) {
+//         if (parent.right && parent.right.value === value) {
+//           // 'current' is the parent's 'right'
+//           if (current.left && current.right) {
+//             // do cool stuff
+//             const min = current.getMinNode();
+//             current.remove(min.value);
+//             current.value = min.value;
+//           } else if (current.left) {
+//             // only has a 'left' child -> set parent's child to current's 'left'
+//             parent.right = current.left;
+//           } else if (current.right) {
+//             // only has a 'right' child -> set parent's child to current's 'right'
+//             parent.right = current.right;
+//           } else {
+//             parent.right = null;
+//           }
+//         } else if (parent.left && parent.left.value === value) {
+//           // 'current' is the parent's 'left'
+//           if (current.left && current.right) {
+//             // do cool stuff
+//             const min = current.getMinNode();
+//             current.remove(min.value);
+//             current.value = min.value;
+//           } else if (current.left) {
+//             // only has a 'left' child -> set parent's child to current's 'left'
+//             parent.left = current.left;
+//           } else if (current.right) {
+//             // only has a 'right' child -> set parent's child to current's 'right'
+//             parent.left = current.right;
+//           } else {
+//             parent.left = null;
+//           }
+//         }
+//       } else {
+//         // this is a root
+//       }
+//     } else if (current.value < value) {
+//       if (current.right) {
+//         traverse(current.right, current);
+//       }
+//     } else {
+//       if (current.left) {
+//         traverse(current.left, current);
+//       }
+//     }
+//   };
+//   traverse(this);
+//   return this;
 // };
-
-BinarySearchTree.prototype.checkIfBalanced = function() {
-  // a tree is balanced if the difference in depths of L sub-tree and R sub-tree is no greater than 1
-  let depthL = this.left ? this.left.findDepth() : 0;
-  let depthR = this.right ? this.right.findDepth() : 0;
-
-  return (Math.abs(depthL - depthR) <= 1);
-};
-
-// let tree = new BinarySearchTree(6);
-// assert.equal(tree.value, 6);
-// assert.equal(tree.contains(6), true);
-// assert.equal(tree.contains(4), false);
-// tree.insert(4);
-// assert.equal(tree.contains(4), true);
-// assert.equal(tree.left.value, 4);
-// assert.equal(tree.right, null);
-// tree.insert(8);
-// assert.equal(tree.right.value, 8);
-// assert.equal(tree.contains(5), false);
-// tree.insert(5);
-// assert.equal(tree.contains(5), true);
-// tree.insert(5).insert(11).insert(7).insert(3);
-
-// let arr = tree.toArray_inOrder();
-// assert.deepEqual(arr, [3, 4, 5, 6, 7, 8, 11]);
-
-// let values = [];
-// tree.traverseDepthFirst_inOrder((node) => values.push(node.value));
-// assert.deepEqual(values, [3, 4, 5, 6, 7, 8, 11]);
-
-// tree = tree.deleteMin();
-// assert.deepEqual(tree.toArray_inOrder(), [4, 5, 6, 7, 8, 11]);
-// tree = tree.deleteMin();
-// assert.deepEqual(tree.toArray_inOrder(), [5, 6, 7, 8, 11]);
-// tree = tree.deleteMax();
-// assert.deepEqual(tree.toArray_inOrder(), [5, 6, 7, 8]);
-// tree = tree.deleteMin();
-// assert.deepEqual(tree.toArray_inOrder(), [6, 7, 8]);
-// tree = tree.deleteMin();
-// assert.deepEqual(tree.toArray_inOrder(), [7, 8]);
-
-// let t2 = new BinarySearchTree(2);
-// t2.insert(4).insert(6).insert(8).insert(7).insert(10).insert(12);
-// assert.deepEqual(t2.toArray_inOrder(), [2, 4, 6, 7, 8, 10, 12]);
-// t2.remove(5); // should do nothing
-// assert.deepEqual(t2.toArray_inOrder(), [2, 4, 6, 7, 8, 10, 12]);
-// t2.remove(10); // should rebuild the tree without 10
-// assert.deepEqual(t2.toArray_inOrder(), [2, 4, 6, 7, 8, 12]);
-// // t2.remove(12) // should rebuild the tree without 12
-// // assert.deepEqual(t2.toArray_inOrder(), [2, 4, 6, 7, 8, 10])
-// t2.remove(8); // should rebuild the tree without 8
-// console.log(t2.toArray_inOrder());
-// assert.deepEqual(t2.toArray_inOrder(), [2, 4, 6, 7, 12]);
--- src/js/DataStructures/BST/bst.spec.js
+++ src/js/DataStructures/BST/bst.spec.js
@@ -4,7 +4,7 @@ describe('BinarySearchTree', () => {
   describe('insert', () => {
     it('returns a new BST with the specified value', () => {
       const bst = new BinarySearchTree(10);
-      let bst2 = bst.insert(15);
+      const bst2 = bst.insert(15);
       expect(bst2 instanceof BinarySearchTree).toBe(true);
       expect(bst2.value).toBe(15);
       expect(bst2.left).toBe(null);
@@ -113,80 +113,204 @@ describe('BinarySearchTree', () => {
   });
 
   describe('remove', () => {
+    it('correctly removes the min node', () => {
+      const bst = new BinarySearchTree(10);
+      bst.insert(5);
+      bst.insert(15);
+      bst.insert(8);
+      bst.insert(12);
+      bst.insert(20);
+      bst.insert(2);
+
+      bst.remove(2);
+
+      let arr = [];
+      bst.traverseDepthFirstInOrder(val => arr.push(val));
+      expect(arr).toEqual([5, 8, 10, 12, 15, 20]);
+
+      arr = [];
+      bst.traverseDepthFirstPreOrder(val => arr.push(val));
+      expect(arr).toEqual([10, 5, 8, 15, 12, 20]);
+
+      arr = [];
+      bst.traverseDepthFirstPostOrder(val => arr.push(val));
+      expect(arr).toEqual([8, 5, 12, 20, 15, 10]);
+    });
+
+    it('correctly removes the max node', () => {
+      const bst = new BinarySearchTree(10);
+      bst.insert(5);
+      bst.insert(15);
+      bst.insert(8);
+      bst.insert(12);
+      bst.insert(20);
+      bst.insert(2);
+
+      bst.remove(20);
+
+      let arr = [];
+      bst.traverseDepthFirstInOrder(val => arr.push(val));
+      expect(arr).toEqual([2, 5, 8, 10, 12, 15]);
+
+      arr = [];
+      bst.traverseDepthFirstPreOrder(val => arr.push(val));
+      expect(arr).toEqual([10, 5, 2, 8, 15, 12]);
+
+      arr = [];
+      bst.traverseDepthFirstPostOrder(val => arr.push(val));
+      expect(arr).toEqual([2, 8, 5, 12, 15, 10]);
+    });
+
+    it('correctly removes a node with a single left child', () => {
+      // TODO: fill this out!
+    });
+
+    it('correctly removes a node with a single right child', () => {
+      // TODO: fill this out!
+    });
+
+    it('correctly removes a node with 2 children', () => {
+      // TODO: fill this out!
+    });
   });
 
-  describe('deleteMin', () => {
+  describe('isFull', () => {
+    it('returns true if root node has zero children', () => {
+      const bst = new BinarySearchTree(10);
+      expect(bst.isFull()).toBe(true);
+    });
+
+    it('returns true if root node has two children', () => {
+      const bst = new BinarySearchTree(10);
+      bst.insert(5);
+      bst.insert(15);
+      expect(bst.isFull()).toBe(true);
+    });
+
+    it('returns false if root node has only a left child', () => {
+      const bst = new BinarySearchTree(10);
+      bst.insert(5);
+      expect(bst.isFull()).toBe(false);
+    });
+
+    it('returns false if root node has only a right child', () => {
+      const bst = new BinarySearchTree(10);
+      bst.insert(15);
+      expect(bst.isFull()).toBe(false);
+    });
+
+    it('returns true if all sub-trees have zero children', () => {
+      const bst = new BinarySearchTree(10);
+      const bstL = bst.insert(5);
+      const bstR = bst.insert(15);
+      expect(bst.isFull()).toBe(true);
+    });
+
+    it('returns true if all sub-trees have two children', () => {
+      const bst = new BinarySearchTree(10);
+      const bstL = bst.insert(5);
+      bstL.insert(4);
+      bstL.insert(6);
+      const bstR = bst.insert(15);
+      bstR.insert(14);
+      bstR.insert(16);
+      expect(bst.isFull()).toBe(true);
+    });
+
+    it('returns false if any sub-tree has only a single child', () => {
+      const bst = new BinarySearchTree(10);
+      const bstL = bst.insert(5);
+      bstL.insert(4);
+      bstL.insert(6);
+      const bstR = bst.insert(15);
+      bstR.insert(14);
+      expect(bst.isFull()).toBe(false);
+    });
   });
 
-  describe('deleteMax', () => {
-  });
+  describe('isBalanced', () => {
+    it('returns true if root node has zero children', () => {
+      const bst = new BinarySearchTree(10);
+      expect(bst.isBalanced()).toBe(true);
+    });
 
-  describe('toArrayInOrder', () => {
+    it('returns true if root node has only two children', () => {
+      const bst = new BinarySearchTree(10);
+      bst.insert(5);
+      bst.insert(15);
+      expect(bst.isBalanced()).toBe(true);
+    });
+
+    it('returns true if root node has only a left child', () => {
+      const bst = new BinarySearchTree(10);
+      bst.insert(5);
+      expect(bst.isBalanced()).toBe(true);
+    });
+
+    it('returns true if root node has only a right child', () => {
+      const bst = new BinarySearchTree(10);
+      bst.insert(15);
+      expect(bst.isBalanced()).toBe(true);
+    });
+
+    it('returns false if left tree is too deep', () => {
+      const bst = new BinarySearchTree(10);
+      bst.insert(5);
+      bst.insert(4);
+      bst.insert(3);
+      bst.insert(11);
+      expect(bst.isBalanced()).toBe(false);
+    });
+
+    it('returns false if right tree is too deep', () => {
+      const bst = new BinarySearchTree(10);
+      bst.insert(5);
+      bst.insert(11);
+      bst.insert(12);
+      bst.insert(13);
+      expect(bst.isBalanced()).toBe(false);
+    });
   });
 
   describe('traverseDepthFirstInOrder', () => {
+    it('traverses the values in the correct order', () => {
+      const bst = new BinarySearchTree(10);
+      bst.insert(5);
+      bst.insert(15);
+      bst.insert(2);
+      bst.insert(8);
+      bst.insert(12);
+      const arr = [];
+      bst.traverseDepthFirstInOrder(val => arr.push(val));
+      expect(arr).toEqual([2, 5, 8, 10, 12, 15]);
+    });
   });
 
   describe('traverseDepthFirstPreOrder', () => {
+    it('traverses the values in the correct order', () => {
+      const bst = new BinarySearchTree(10);
+      bst.insert(5);
+      bst.insert(15);
+      bst.insert(2);
+      bst.insert(8);
+      bst.insert(12);
+      const arr = [];
+      bst.traverseDepthFirstPreOrder(val => arr.push(val));
+      expect(arr).toEqual([10, 5, 2, 8, 15, 12]);
+    });
   });
 
   describe('traverseDepthFirstPostOrder', () => {
-  });
-
-  // describe('checkIfFull', () => {
-  //   it('returns true if root node has zero children', () => {
-  //     const bst = new BinarySearchTree(10);
-  //     expect(bst.checkIfFull()).toBe(true);
-  //   });
-
-  //   it('returns true if root node has two children', () => {
-  //     const bst = new BinarySearchTree(10);
-  //     bst.insert(5);
-  //     bst.insert(15);
-  //     expect(bst.checkIfFull()).toBe(true);
-  //   });
-
-  //   it('returns true if root node has on a left child', () => {
-  //     const bst = new BinarySearchTree(10);
-  //     bst.insert(5);
-  //     expect(bst.checkIfFull()).toBe(false);
-  //   });
-
-  //   it('returns true if root node has on a right child', () => {
-  //     const bst = new BinarySearchTree(10);
-  //     bst.insert(15);
-  //     expect(bst.checkIfFull()).toBe(false);
-  //   });
-
-  //   it('returns true if all sub-trees have zero children', () => {
-  //     const bst = new BinarySearchTree(10);
-  //     const bstL = bst.insert(5);
-  //     const bstR = bst.insert(15);
-  //     expect(bst.checkIfFull()).toBe(true);
-  //   });
-
-  //   it('returns true if all sub-trees have two children', () => {
-  //     const bst = new BinarySearchTree(10);
-  //     const bstL = bst.insert(5);
-  //     bstL.insert(4);
-  //     bstL.insert(6);
-  //     const bstR = bst.insert(15);
-  //     bstR.insert(14);
-  //     bstR.insert(16);
-  //     expect(bst.checkIfFull()).toBe(true);
-  //   });
-
-  //   it('returns true if any sub-tree has only a single child', () => {
-  //     const bst = new BinarySearchTree(10);
-  //     const bstL = bst.insert(5);
-  //     bstL.insert(4);
-  //     bstL.insert(6);
-  //     const bstR = bst.insert(15);
-  //     bstR.insert(14);
-  //     expect(bst.checkIfFull()).toBe(false);
-  //   });
-  // });
-
-  describe('checkIfBalanced', () => {
+    it('traverses the values in the correct order', () => {
+      const bst = new BinarySearchTree(10);
+      bst.insert(5);
+      bst.insert(15);
+      bst.insert(2);
+      bst.insert(8);
+      bst.insert(12);
+      const arr = [];
+      bst.traverseDepthFirstPostOrder(val => arr.push(val));
+      expect(arr).toEqual([2, 8, 5, 12, 15, 10]);
+    });
   });
 });
